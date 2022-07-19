aed25a198d7347cd3dd0794235a5da9a83c3f9ea
hannaA <106862301+HannahA88@users.noreply.github.com>
2022-07-19 23:38:25 +0200

    add function randomColor

--- Test JS function/index.js
+++ Test JS function/index.js
@@ -2,7 +2,8 @@
 const container = document.getElementById("container");
 container.style.backgroundColor = "pink";
 
-function backgroundToBlue(event) {
+
+/*function backgroundToBlue(event) {
     event.target.style.backgroundColor = "blue";
     console.log(event.target);
 }
@@ -11,6 +12,23 @@ function backgroundToPink(event) {
     event.target.style.backgroundColor = "pink";
     console.log(event.target);
 }
+*/
 
-container.addEventListener("mouseover", backgroundToBlue);
-container.addEventListener("mouseleave", backgroundToPink);
+function backgroundToNext(event) {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    event.target.style.backgroundColor = "#" + randomColor;
+    console.log(event.target);
+}
+
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+}
+
+
+/*container.addEventListener("mouseover", backgroundToBlue);
+container.addEventListener("mouseleave", backgroundToPink);*/
+
+container.addEventListener("mouseover", setBg);
+container.addEventListener("mouseover", backgroundToNext);
